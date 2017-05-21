a2b59f61ae5349e200f29647bbffeccb86e89eaa
Alex Liotta <aliotta343@gmail.com>
2017-05-20 17:51:34 -0700

    heroku

--- db/config.js
+++ db/config.js
@@ -1,10 +1,29 @@
-var dbConfig = {
+var dbConfig;
+
+if(process.env.PRODUCTION === 'TRUE') {
+  var url = require('url');
+  var util = require('util');
+  var env_url = process.env.DATABASE_URL;
+  var db_url = url.parse(env_url);
+  var auth = db_url.auth.split(':');
+  dbConfig = {
+    database: db_url.pathname.split('/')[1],
+    user: auth[0],
+    password: auth[1],
+    host: db_url.hostname,
+    port: db_url.port,
+    ssl: true
+  };
+}
+else {
+  dbConfig = {
     database: 'postgres',
     schema: 'appt',
     user: 'postgres',
     password: 'cheese',
     host: 'localhost',
     port: 5432
-};
+  };
+}
 
 module.exports = dbConfig;
